961ad1b444ea77a16a229e794889deb52fe24e3a
Lisa <300083632@fvtc.edu>
2023-09-15 00:31:29 -0500

    Homepage video

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+    <head>
+            <meta charset="utf-8">
+            <title>Lisa's Homepage</title>
+            <link rel="stylesheet" href="css/base.css">
+    </head>
+    <body>
+        <header>Lisa's Website</header>
+        <nav>
+            <ul>
+                <li><a href="/">Homepage</a></li>
+                <li><a href="#">Loop</a></li>
+                <li><a href="#">Countdown</a></li>
+            </ul>
+        </nav>
+        <main>
+            <img src="images/lisa2.jpg" alt="picture of Lisa" />
+            <p>My name is Paj Xiav Yang, but I go by Lisa. I am a student at FVTC
+            and my major is Software Development. In my free time I like to play video games.
+            Some games I play is Diablo, Warzone, Fortnite and some indie games. I also like
+            to watch movies, play the piano, and go on walks with my dog. I like to watch
+            horror movies, my favorite series is The Nun series. I like them because I find them
+            exciting and there is always easter eggs with lots of lore.</p>
+        </main>
+        <footer>&copy; 2023 LisaYang.com</footer>
+    </body>
+</html>
